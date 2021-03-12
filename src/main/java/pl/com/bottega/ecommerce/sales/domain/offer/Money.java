212a1b555a4e844e43a3045b3324dbc5f838c144
Mmati1996 <mat.duk.at@gmail.com>
2021-03-12 16:43:35 +0100

    created money class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -0,0 +1,16 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+public class Money {
+    private String currency;
+    private double amount;
+
+    public Money(String currency, double amount) {
+        this.currency = currency;
+        this.amount = amount;
+    }
+
+    public Money(double amount) {
+        this.currency = "euro";
+        this.amount = amount;
+    }
+}
